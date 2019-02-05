22439ef10f8e46de65fdd158160a38a3aa87c507
Peter Emmanuel Orlando <peter.emmanuel.orlando@gmail.com>
2019-02-05 14:28:15 -0500

    finished Hw

--- js_questions.js
+++ js_questions.js
@@ -111,50 +111,44 @@ printHeader(`
 // 	<input id="num2" class="nums" type="text"/>
 // 	<h3>Sum: <span id="sum"></span></h3>  
 `);
-function getSumEvent()
+function setUpSumEvent()
 {
 	let span = document.getElementById("sum");
 	let spanParent = span.parentElement;
 	//do logic
-	let input1 = document.createElement("input");
-	let plus = document.createElement("span");
-	let input2 = document.createElement("input");
-
-	input1.value = 1;
-	plus.innerHTML = " + "
-	input2.value = 2;
-
-	let val1 = input1.value;
-	let val2 = input2.value;
-
-	spanParent.insertBefore(input1, span);
-	spanParent.insertBefore(plus, span);
-	spanParent.insertBefore(input2, span);
-
-	span.innerHTML = ` = ${+val1 + +val2}`;
-
-	console.log( `sum is ${+val1 + +val2}`)
+	let input1 = document.getElementById("num1");
+	let input2 = document.getElementById("num2");
+	let sum = function(){ 
+		let result = +input1.value + +input2.value;
+		span.innerText = result || 'Cannot add'
+	}
+	input1.addEventListener("input", sum);
+	input2.addEventListener("input", sum);
 }
-getSumEvent();
-  
+setUpSumEvent();
+
+
 printHeader(`
 // Define onchange event handler.
 // Add <input> element values.
 // Put the sum in the <span> element.
 // If values cannot be added, put "Cannot add" in the <span> element
-`);
-function onChangeEvent()
-{
-	
-}
-onChangeEvent();
 
 // 7. Skills Event
 // NOTE: Write unobtrusive Javascript
 // When user selects a skill, create an alert with a message similar to:
 // 	"Are you sure CSS is one of your skills?"
 // NOTE: no alert should appear when user deselects a skill.
+`);
+function setUpAlertEvent()
+{
+	var skillSelect = document.getElementsByName("skills")[0];
+	var skill = () =>{ return skillSelect.options[skillSelect.selectedIndex].text;}
+	skillSelect.addEventListener("change", ()=>{alert(`Are you sure ${skill()} is one of your skills?`)});	
+}
+setUpAlertEvent();
 
+printHeader(`
 // 8. Favorite Color Event
 // NOTE: Write unobtrusive Javascript
 // NOTE: This is regarding the favoriteColor radio buttons.
@@ -162,25 +156,84 @@ onChangeEvent();
 // 	"So you like green more than blue now?"
 // In this example, green is the new value and blue is the old value.
 // Make the background color (of all favoriteColor radio buttons) the newly selected favoriteColor
+`);
+function setUpFavoriteColorEvent()
+{
+	var colorButtons = document.getElementsByName("favoriteColor");
+	let currentFavorite = 'nothing';
+	colorButtons.forEach(e => {
+		e.addEventListener("change", ()=>{alert(`So you like ${e.value} more than ${currentFavorite} now?`); currentFavorite = e.value});
+	});	
+}
+setUpFavoriteColorEvent();
 
+printHeader(`
 // 9. Show/Hide Event
 // NOTE: Write unobtrusive Javascript
 // When user hovers over an employees name:
 // 	Hide the name if shown.
 // 	Show the name if hidden.
+`);
+function setUpShowHideEvent()
+{
+	var nameElems = document.getElementsByClassName("empName");
+	for (const e of nameElems) {
+		e.addEventListener("mouseover", ()=>{
+			if(e.style.opacity == 0)
+				e.style.opacity = 1;
+			else
+				e.style.opacity = 0;
+		});
+	}
+}
+setUpShowHideEvent();
 
+printHeader(`
 // 10. Current Time
 // Regarding this element:
 // 	<h5 id="currentTime"></h5>
 // Show the current time in this element in this format: 9:05:23 AM
 // The time should be accurate to the second without having to reload the page.
+`);
+function setUpTime()
+{
+	var time = document.getElementById("currentTime");
+	setInterval(() => {
+		let d = new Date();
+		time.innerText = `the current time is: ${d.getHours() % 12}:${d.getMinutes()}:${d.getSeconds()} ${d.getHours()<12?'am':'pm'}`;		
+	}, 1000);
+}
+setUpTime();
 
+printHeader(`
 // 11. Delay
 // Regarding this element:
 // 	<p id="helloWorld">Hello, World!</p>
 // Three seconds after a user clicks on this element, change the text to a random color.
+`);
+function setUpColorChangeEvent()
+{
+	var text = document.getElementById("helloWorld");
+	text.addEventListener("click", ()=>{
+		setTimeout(() => {
+			text.style.color = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`; 	
+		}, 3000);
+	});
+}
+setUpColorChangeEvent();
 
+printHeader(`
 // 12. Walk the DOM
 // Define function walkTheDOM(node, func)
 // This function should traverse every node in the DOM. Use recursion.
 // On each node, call func(node).
+`, true);
+function walkTheDOM(startElement, func)
+{
+	func(startElement)
+	for (const child of startElement.childNodes) {
+		walkTheDOM(child, func)
+	}
+}
+let func = ()=>{console.log(`We're in the Func()!!`)}
+walkTheDOM(document, func);
